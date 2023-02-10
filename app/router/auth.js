5dca73a0284cb5e14b1d550e37e949c92b73d42c
mahdi hasanpour <mahdi.hasanpour2022@gmail.com>
2023-02-10 10:12:51 -0800

    post to auth/register route

--- app/router/auth.js
+++ app/router/auth.js
@@ -1,6 +1,12 @@
 // step 20 : import express router and create auth router
-const express = require('express')
+const express = require('express');
 const router = express.Router();
 
-module.exports = {authRoutes : router}
+const { AuthController } = require('../http/controllers/auth.controller');
+const { registerValidator } = require('../http/validations/authValidation');
+
+// step 41 : estefade az function 
+router.post("./register", registerValidator(), AuthController.register)
+
+module.exports = { authRoutes: router }
 
